a9c6d707930467abe72a0486231c737d19923582
pedram.enayati.kashani <pedram.en1376@gmail.com>
2024-07-28 17:12:24 +0330

    finish

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
-<?php 
+<?php
 
-$token = "7189598533:AAF4h1d-d1fMs6j1vJPb3hMs9MguHODQaaA";
-$action = '/getUpdates';
-$url = "https://api.telegram.org/bot" . $token.$action;
-$result = file_get_contents($url);
-echo $result;
+$content = file_get_contents('php://input');
+$update = json_decode($content, true);
+$text = $update['message']['text'];
+switch ($text) {
+    case "/start":
+        $sentMessage = 'به ربات من خوش آمدید';
+        break;
+    case "/command1":
+        $sentMessage = 'شما کامند ۱ را انتخاب کردید';
+        break;
+    case "/command2":
+        $sentMessage = 'شما کامند ۲ را انتخاب کردید';
+        break;
+    default:
+        $sentMessage = 'هیچ پیغامی برای نمایش به شما وجود ندارد لطفا کامند درستی انتخاب نمایید';
+}
+$chat_id = $update['message']['chat']['id'];
+$content = urlencode($content);
+$result = "https://api.telegram.org/bot_______/sendmessage?chat_id=" . $chat_id . "&text=" . $sentMessage;
+file_get_contents($result);
